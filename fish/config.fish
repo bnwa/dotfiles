35459f939c74b7f2081cfe6f4d5ce3e9312ae3bf
Benoît Barberousse <heybenoit@gmail.com>
2020-11-24 16:59:10 -0600

    Condition Java, Python config

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,17 @@
-fish_vi_key_bindings
+set -l hasJava (ls /Library/Java/JavaVirtualMachines 2> /dev/null)
+set -l hasPython (ls ~/Library/Python 2> /dev/null)
 
-set -g JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home
-set -g PATH ~/Library/Python/3.8/bin $JAVA_HOME/bin $PATH
+if [ $hasJava ]
+  set -g JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home
+  set -g PATH $JAVA_HOME/bin $PATH
+end
+
+if [ $hasPython ] && test -d /Library/Python/"$hasPython[1]"/bin
+  set -g PATH ~/Library/Python/3.8/bin $PATH
+end
+
+fish_vi_key_bindings
 
 for mode in insert default visual
   bind -M $mode \cf forward-char
 end
-
-function fish_prompt
-  printf "%s %s" (set_color $fish_color_cwd)(basename (pwd))(set_color red) λ (set_color normal)
-end
-
-
