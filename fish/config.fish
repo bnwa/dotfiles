9d048f30ba41d4c2cc9477b92d2b601b9dd9d2bd
Benoît Barberousse <heybenoit@gmail.com>
2020-11-24 17:24:23 -0600

    Fix USER_PYTHON refeference

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set -l SYS_JAVA /Library/Java/JavaVirtualMachines
 set -l USER_PYTHON ~/Library/Python
 
 set -l hasJava (ls $SYS_JAVA 2> /dev/null)
-set -l hasPython (ls USER_PYTHON 2> /dev/null)
+set -l hasPython (ls $USER_PYTHON 2> /dev/null)
 
 if [ "$hasJava" ]
   set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home
