6d1fff2755a2506ab69c9befd87d3e57fd39443d
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-05-11 11:25:24 -0500

    Correct directory tests

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,16 @@
-if test -f /opt/homebrew
+if test -d /opt/homebrew
   set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib"
   set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include"
   fish_add_path -U /opt/homebrew/bin
   fish_add_path -U /opt/homebrew/opt/node@16/bin
 end
+
+if test -d /usr/local/Homebrew
+  set -gx LDFLAGS "-L/usr/local/opt/node@16/lib"
+  set -gx CPPFLAGS "-I/usr/local/opt/node@16/include"
+  fish_add_path /usr/local/opt/node@16/bin
+end
+
 
 fish_vi_key_bindings
 
