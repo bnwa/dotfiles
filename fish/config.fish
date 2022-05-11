1f384b7267cc3fb3f5a45591acac30dcd2d4b870
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-05-10 23:48:14 -0500

    Automate M1 homebrew vars

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
-if test -f /opt/homebrew;
-  fish_add_path /opt/homebrew/bin
+if test -f /opt/homebrew
+  set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib"
+  set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include"
+  fish_add_path -U /opt/homebrew/bin
+  fish_add_path -U /opt/homebrew/opt/node@16/bin
 end
 
 fish_vi_key_bindings
