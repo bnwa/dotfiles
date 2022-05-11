32cc1b98d98db1b63c3db383709f519760e38a10
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-05-10 21:27:20 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
-fish_add_path /usr/local/opt/node@14/bin
+if test -f /opt/homebrew;
+  fish_add_path /opt/homebrew/bin
+end
 
 fish_vi_key_bindings
 
 for mode in insert default visual
   bind -M $mode \cf forward-char
 end
+
+set -xU nvm_default_version lts/gallium
