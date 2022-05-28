d029b09a7889e5ecbedb6f885e263bd46b4ce3b1
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-05-27 20:20:28 -0500

    Rename install_path to packer_path in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,10 +6,10 @@ local opt = vim.opt
 local cmd = vim.cmd
 local ex = api.nvim_command
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
-if fn.isdirectory(fn.glob(install_path)) == 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', install_path})
+if fn.isdirectory(fn.glob(packer_path)) == 0 then
+  fn.system({'git', 'clone', '--depth=1', 'https://github.com/wbthomason/packer.nvim', packer_path})
 end
 
 opt.background = "light"
