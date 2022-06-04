3371c0a742bd943bda90f58beba82c62df67575f
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-06-04 17:51:50 -0500

    nvim - Fix number as window opt, wrap autocmds in group

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,17 +1,20 @@
 local bo = vim.bo
+local wo = vim.wo
 local new = vim.api.nvim_create_autocmd
 
-new({ 'TermOpen' }, { pattern = '*', callback = function()
-  bo.number = false
-  bo.relativenumber = false
+local user = vim.api.nvim_create_augroup('user', { clear = true })
+
+new({ 'TermOpen' }, { pattern = '*', group = user, callback = function()
+  wo.number = false
+  wo.relativenumber = false
 end })
 
-new({ 'VimEnter' }, { pattern = '*.md', callback = function()
+new({ 'VimEnter' }, { pattern = '*.md', group = user, callback = function()
   bo.textwidth = 80
   bo.wrap = true
 end })
 
-new({ 'FileType' }, { pattern = '*.fish', callback = function()
+new({ 'FileType' }, { pattern = '*.fish', group = user, callback = function()
   bo.tabstop = 4
   bo.softtabstop = 4
   bo.shiftwidth = 4
