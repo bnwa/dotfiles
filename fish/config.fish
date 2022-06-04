6a1dc1e27d3272634db0a3af138b505bab6c5e12
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-06-04 17:53:02 -0500

    fish - make sure dir tests ensure dirs readable

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,17 @@
-if test -d /opt/homebrew
+if test -r /opt/homebrew
   set -gx LDFLAGS "-L/opt/homebrew/opt/node@16/lib"
   set -gx CPPFLAGS "-I/opt/homebrew/opt/node@16/include"
   fish_add_path -U /opt/homebrew/bin
   fish_add_path -U /opt/homebrew/opt/node@16/bin
 end
 
-if test -d /usr/local/Homebrew
+if test -r /usr/local/Homebrew
   set -gx LDFLAGS "-L/usr/local/opt/node@16/lib"
   set -gx CPPFLAGS "-I/usr/local/opt/node@16/include"
   fish_add_path /usr/local/opt/node@16/bin
 end
+
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_331.jdk/Contents/Home
 
 
 fish_vi_key_bindings
