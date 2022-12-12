b17e84cbce1377712368c31611850e877baa20c2
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-12-12 00:25:33 -0600

    Add floor to is_night

--- nvim/lua/lib.lua
+++ nvim/lua/lib.lua
@@ -32,7 +32,7 @@ function M.is_night()
   local date_str = os.date()
   local date_parts = vim.split(date_str, '[%s%p]')
   local date_hour = vim.fn.str2nr(date_parts[4])
-  if date_hour > 17 then return true else return false end
+  if date_hour > 17 or date_hour < 7 then return true else return false end
 end
 
 return M
