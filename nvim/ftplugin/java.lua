f07d8ec3b199e0da411f5e0c107de66faca9de86
Benoît Barberousse <74591246+bnwa@users.noreply.github.com>
2022-12-22 21:57:18 -0600

    Configure jdtls runtimes settings conditionally

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,3 +1,5 @@
+local fn = vim.fn
+local util = require 'util'
 local ok, jdtls = pcall(require, 'jdtls')
 local cmp_ok, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
 
@@ -12,14 +14,31 @@ local project_root = jdtls.setup.find_root { 'pom.xml', '.git' }
 local capabilities = vim.tbl_deep_extend('force',
   vim.lsp.protocol.make_client_capabilities(),
   cmp_lsp.default_capabilities())
-local util = require 'util'
-local fn = vim.fn
+
+local home_path = os.getenv 'HOME'
+
+local sdkman_root = home_path .. '/.sdkman'
+local sdkman_java_11 = sdkman_root .. '/candidates/java/11.0.17-zulu'
+
+local oracle_jre_root = '/Library/Java/JavaVirtualMachines'
+local oracle_java_8 = oracle_jre_root .. '/jdk1.8.0_331.jdk/Contents/Home'
+
+local runtimes = {}
+if util.is_directory(sdkman_java_11) then table.insert(runtimes, { name = 'JavaSE-11', path = sdkman_java_11 }) end
+if util.is_directory(oracle_java_8) then table.insert(runtimes, { name = 'JavaSE-1.8', path = oracle_java_8 }) end
+
 
 if not util.is_directory(workspace_root) then fn.mkdir(workspace_root, 'p') end
 
 local config = { capabilities = capabilities }
 
-config.settings = {}
+config.settings = {
+  java = {
+    configuration = {
+      runtimes = runtimes,
+    },
+  },
+}
 config.init_options = {
   extended_capabilities = jdtls.extendedClientCapabilities
 }
