a042522a256a67ade314fddef2b1518c4afb5913
Benoît Barberousse <benoit@Benoits-MBP.hsd1.il.comcast.net>
2022-12-24 16:44:26 -0600

    Toggle background based on day/night detection on focus gain/loss

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,6 +1,8 @@
 local bo = vim.bo
 local wo = vim.wo
+local opt = vim.opt
 local new = vim.api.nvim_create_autocmd
+local util = require 'util'
 
 local user = vim.api.nvim_create_augroup('user', { clear = true })
 
@@ -18,4 +20,12 @@ new({ 'FileType' }, { pattern = '*.fish', group = user, callback = function()
   bo.tabstop = 4
   bo.softtabstop = 4
   bo.shiftwidth = 4
+end })
+
+new({ 'FocusGained', 'FocusLost' }, { group = user, callback = function()
+  if util.is_night() then
+    opt.background = 'dark'
+  else
+    opt.background = 'light'
+  end
 end })
